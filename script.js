f4286e3b932472f4758ff129d7d463be44b57d77
Brian Butler <butlerdavidbrian@gmail.com>
2020-09-25 09:47:16 -0400

    cha cha changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict"
 
 window.onload = function(){
-    letsgo();
+    letsGo();
 }
 
 class Node{
@@ -19,18 +19,11 @@ class LinkedList{
 }
 
 LinkedList.prototype.insertAtBeginning = function(row, col){
-// A newNode object is created with property data and next = null
     let newNode = new Node(row, col);
-// The pointer next is assigned head pointer so that both pointers now point at the same node.
     newNode.next = this.head;
-// As we are inserting at the beginning the head pointer needs to now point at the newNode.
-
     this.head = newNode;
-
     return this.head;
 }
-
-
 
 LinkedList.prototype.deleteLastNode = function(){
 
@@ -55,7 +48,8 @@ LinkedList.prototype.deleteLastNode = function(){
     return this.head;
 }
 
-function letsgo(){
+//Main Function
+function letsGo(){
         document.addEventListener('keypress', logKey);
 
         function logKey(e) {
@@ -77,68 +71,63 @@ function letsgo(){
         }
 
         let list = new LinkedList();
-
-        let gameoverflag = false;
+        let gameOverFlag = false;
         let ctx = document.getElementById('grid').getContext('2d');
+        let emptyColor = "rgb(100,45,0)";
+        let snakeColor = "rgb(10,45,134)";
+        let foodColor = "rgb(0,245,100)";
+        let headColor = "rgb(0,100,100)";
+        let tailColor = "rgb(0,100,0)";
+        let foodRow;
+        let foodColumn;
+        let rowNumber = 19;
+        let columnNumber = 19;
+        let cellSize = 21;
+        let innerCellSize = 20;
 
-        let emptycolor = "rgb(100,45,0)";
-        let snakecolor = "rgb(10,45,134)";
-        let foodcolor = "rgb(0,245,100)";
-        let headcolor = "rgb(0,100,100)";
-        let tailcolor = "rgb(0,100,0)";
-        let foodrow;
-        let foodcol;
-        let rownum = 19;
-        let colnum = 19;
-        let cellsize = 21;
-        let inndercellsize = 20;
+        initializeSnake();
+        drawBoard();
+        randomFood();
+        drawFood();
+        drawSnake();
 
+        function initializeSnake() {
+            let snakeRow = Math.floor(Math.random() * rowNumber);
+            let snakeColumn = Math.floor(Math.random() * columnNumber);
 
-        initializesnake();
-        drawboard();
-        randomfood();
-        drawfood();
-        drawsnake();
-
-        function initializesnake() {
-            let snakerow;
-            snakerow = Math.floor(Math.random() * rownum);
-            let snakecol;
-            snakecol = Math.floor(Math.random() * colnum);
-
-            list.insertAtBeginning(snakerow, snakecol);
+            list.insertAtBeginning(snakeRow, snakeColumn);
         }
 
-        function randomfood() {
+        function randomFood() {
             let agent = list.head;
-            foodrow = Math.floor(Math.random() * rownum);
-            foodcol = Math.floor(Math.random() * colnum);
+            foodRow = Math.floor(Math.random() * rowNumber);
+            foodColumn = Math.floor(Math.random() * columnNumber);
             while (agent != null) {
-                if (foodrow === agent.row && foodcol === agent.col) {
-                    randomfood();
+                if (foodRow === agent.row && foodColumn === agent.col) {
+                    randomFood();
                 }
                 agent = agent.next;
             }
         }
 
-        function drawfood() {
-            ctx.fillStyle = foodcolor;
+        function drawFood() {
+            ctx.fillStyle = foodColor;
             ctx.beginPath();
-            let x = foodcol * cellsize;
-            let y = foodrow * cellsize;
-            ctx.rect(x, y, inndercellsize, inndercellsize);
+            let x = foodColumn * cellSize;
+            let y = foodRow * cellSize;
+            ctx.rect(x, y, innerCellSize, innerCellSize);
             ctx.fill();
             ctx.closePath();
         }
 
-        function drawboard() {
-            ctx.fillStyle = emptycolor;
+        function drawBoard() {
+            ctx.fillStyle = emptyColor;
             ctx.beginPath();
 
-            for (let row = 0, i = 0; i < rownum; row += cellsize, i++) {
+            for (let row = 0, i = 0; i < rowNumber; row += cellSize, i++) {
 
-                for (let col = 0, j = 0; j < colnum; col += cellsize, j++) {
-                    ctx.rect(row, col, inndercellsize, inndercellsize);
+                for (let col = 0, j = 0; j < columnNumber; col += cellSize, j++) {
+                    ctx.rect(row, col, innerCellSize, innerCellSize);
                 }
             }
             ctx.fill();
@@ -146,24 +135,25 @@ function letsgo(){
         }
 
         let paragraph = document.querySelector('p');
-        let s = document.getElementById('start')
+        let startButton = document.getElementById('start')
 
-        if (s) {
-            s.addEventListener('click', run, false);
+        if (startButton) {
+            startButton.addEventListener('click', run, false);
         }
-        console.log(s, paragraph)
+        console.log(startButton, paragraph)
 
         function run() {
-            if (s.value === 'Start') {
-                s.value = 'Stop';
+            if (startButton.value === 'Start') {
+                startButton.value = 'Stop';
 
-                setInterval(drawsnake, 300);
-                setInterval(growsnake, 300);
+                setInterval(drawSnake, 400);
+                setInterval(growSnake, 400);
 
-                paragraph.textContent = 'The game has started!';
+                paragraph.textContent = 'The Game has started!';
             } else {
-                s.value = 'Start';
-                paragraph.textContent = 'The Game is stopped.';
+                startButton.value = 'Start';
+                paragraph.textContent = 'The Game is stopped! Click Reset to start a new game!';
+                gameOverFlag = true;
             }
         }
 
@@ -173,9 +163,9 @@ function letsgo(){
             return row1 === row2 && col1 === col2;
         }
 
-        function hitfood(row, col) {
+        function hitFood(row, col) {
 
-            return !!collide(row, col, foodrow, foodcol);
+            return !!collide(row, col, foodRow, foodColumn);
         }
 
         function hitSelfOrWall(newrow, newcol) {
@@ -184,14 +174,14 @@ function letsgo(){
             while (agent != null) {
                 if (collide(newrow, newcol, agent.row, agent.col)) {
                     alert("You hit yourself game over!");
-                    s.value = 'Start';
-                    paragraph.textContent = 'The Game is stopped.';
+                    startButton.value = 'Start';
+                    paragraph.textContent = 'The Game is stopped! Click Reset to start a new game!';
                     return true;
                 }
-                if (agent.row >= rownum || agent.col >= colnum || agent.row < 0 || agent.col < 0) {
+                if (agent.row >= rowNumber || agent.col >= columnNumber || agent.row < 0 || agent.col < 0) {
                     alert("You went off the board!");
-                    s.value = 'Start';
-                    paragraph.textContent = 'The Game is stopped.';
+                    startButton.value = 'Start';
+                    paragraph.textContent = 'The Game is stopped! Click Reset to start a new game!';
                     return true;
                 }
 
@@ -200,65 +190,64 @@ function letsgo(){
             return false;
         }
 
-        function growsnake() {
-            if (gameoverflag === true) {
-                list.head = null;
+        function growSnake() {
+            if (gameOverFlag === true) {
+                startButton.removeEventListener('click', run, false);
                 return;
             }
-            let newcol;
-            let newrow;
+            let newColumn;
+            let newRow;
             switch (direction) {
                 case 'up':
-                    newrow = list.head.row - 1;
-                    newcol = list.head.col;
+                    newRow = list.head.row - 1;
+                    newColumn = list.head.col;
                     break;
                 case 'left':
-                    newrow = list.head.row;
-                    newcol = list.head.col - 1;
+                    newRow = list.head.row;
+                    newColumn = list.head.col - 1;
 
                     break;
                 case 'down':
-                    newrow = list.head.row + 1;
-                    newcol = list.head.col;
+                    newRow = list.head.row + 1;
+                    newColumn = list.head.col;
                     break;
                 case 'right':
-                    newrow = list.head.row;
-                    newcol = list.head.col + 1;
+                    newRow = list.head.row;
+                    newColumn = list.head.col + 1;
             }
 
-            if (hitSelfOrWall(newrow, newcol)) {
-                gameoverflag = true;
+            if (hitSelfOrWall(newRow, newColumn)) {
+                gameOverFlag = true;
             }
 
+            list.insertAtBeginning(newRow, newColumn);
 
-            list.insertAtBeginning(newrow, newcol);
-
-
-            if (!hitfood(newrow, newcol))
+            if (!hitFood(newRow, newColumn))
                 list.deleteLastNode();
             else {
-                randomfood();
-                drawfood();
+                randomFood();
+                drawFood();
             }
         }
 
-        function drawsnake() {
-            if (gameoverflag === true) {
-                list.head = null;
+        function drawSnake() {
+            if (gameOverFlag === true) {
+                startButton.removeEventListener('click', run, false);
                 return;
             }
 
-            drawboard();
-            drawfood();
+            drawBoard();
+            drawFood();
             let x, y;
+
             //Draw Body
             ctx.beginPath();
-            ctx.fillStyle = snakecolor;
+            ctx.fillStyle = snakeColor;
             let agent = list.head;
             while (agent != null) {
-                y = agent.row * cellsize;
-                x = agent.col * cellsize;
-                ctx.rect(x, y, inndercellsize, inndercellsize);
+                y = agent.row * cellSize;
+                x = agent.col * cellSize;
+                ctx.rect(x, y, innerCellSize, innerCellSize);
                 agent = agent.next;
             }
             ctx.fill();
@@ -266,24 +255,24 @@ function letsgo(){
 
             //Draw Tail
             ctx.beginPath();
-            ctx.fillStyle = tailcolor;
+            ctx.fillStyle = tailColor;
             agent = list.head;
             while (agent.next != null) {
                 agent = agent.next;
             }
-            y = agent.row * cellsize;
-            x = agent.col * cellsize;
-            ctx.rect(x, y, inndercellsize, inndercellsize);
+            y = agent.row * cellSize;
+            x = agent.col * cellSize;
+            ctx.rect(x, y, innerCellSize, innerCellSize);
             ctx.fill();
             ctx.closePath();
 
             //Draw Head
             ctx.beginPath();
-            ctx.fillStyle = headcolor;
+            ctx.fillStyle = headColor;
             agent = list.head;
-            y = agent.row * cellsize;
-            x = agent.col * cellsize;
-            ctx.rect(x, y, inndercellsize, inndercellsize);
+            y = agent.row * cellSize;
+            x = agent.col * cellSize;
+            ctx.rect(x, y, innerCellSize, innerCellSize);
             ctx.fill();
             ctx.closePath();
         }
